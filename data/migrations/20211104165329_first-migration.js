50169559baa0f3a2d319be11e970adb21d64f844
Ladrillo <gabriel@cabrejas.net>
2021-11-04 17:58:23 +0100

    wip

--- data/migrations/20211104165329_first-migration.js
+++ data/migrations/20211104165329_first-migration.js
@@ -1,8 +1,9 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable()
-    .createTable()
-    .createTable()
+    .createTable('zoos')
+    .createTable('')
+    .createTable('')
+    .createTable('')
 };
 
 exports.down = function(knex) {
