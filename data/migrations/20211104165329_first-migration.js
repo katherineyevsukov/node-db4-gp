0ab7c6c9c79412a6925714625e7a8b189d177301
Ladrillo <gabriel@cabrejas.net>
2021-11-04 18:00:53 +0100

    wip

--- data/migrations/20211104165329_first-migration.js
+++ data/migrations/20211104165329_first-migration.js
@@ -1,16 +1,16 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('zoos', table => {
-      
+      table.increments() // id
     })
     .createTable('species', table => {
-      
+      table.increments() // id
     })
     .createTable('animals', table => {
-      
+      table.increments() // id
     })
     .createTable('zoo_animals', table => {
-      
+      table.increments() // id
     })
 };
 
